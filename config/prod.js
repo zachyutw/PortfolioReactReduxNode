989255cf53aa67c8495ce0aae33ff116be93d327
Zach Yu <zachyu.tw@gmail.com>
2017-12-27 14:00:29 -0800

    finished auth flow2

---
+++ config/prod.js
@@ -0,0 +1,13 @@
+module.exports = {
+   
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY,
+    /** 
+    mongoURI:'mongodb://sa:!234qwer@ds131697.mlab.com:31697/stepheng-study-prod',
+    googleClientID:'1033193057807-md6eklrpda2mrrnrgbiegc83783gpga3.apps.googleusercontent.com',
+    googleClientSecret:'YRmqLrQ-_LwjnOnaZgOrGjfz',
+    cookieKey:'fjaweiofjowaj2o3o2iojf32ojf2o3',
+    */
+}
